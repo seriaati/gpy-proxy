1dbdbd88b8ac7ed7dcd42dda9e277d0fa06e23ba
seria <seria.ati@gmail.com>
2025-02-03 14:07:19 +0900

    refactor: Remove custom request handling in GenshinClient

--- app/models.py
+++ app/models.py
@@ -1,38 +1,10 @@
 import genshin
 from pydantic import BaseModel
 import typing
-import aiohttp
-import random
-import asyncio
 
 MAX_RETRIES = 3
 BACKOFF_FACTOR = 2
 MAX_BACKOFF = 32
-
-
-class GenshinClient(genshin.Client):
-    async def request(self, *args: typing.Any, **kwargs: typing.Any) -> typing.Any:
-        attempt = 0
-        err: Exception | None = None
-
-        while attempt < MAX_RETRIES:
-            try:
-                return await super().request(*args, **kwargs)
-            except (TimeoutError, aiohttp.ClientError) as e:
-                # Attempt to retry request
-                err = e
-                attempt += 1
-
-                backoff_time = min(
-                    BACKOFF_FACTOR**attempt + random.uniform(0, 1), MAX_BACKOFF
-                )
-                await asyncio.sleep(backoff_time)
-            except Exception:
-                # Raise immediately for other exceptions
-                raise
-
-        msg = f"genshin.py client request failed after {MAX_RETRIES} attempts"
-        raise RuntimeError(msg) from err
 
 
 class BaseAPIRequest(BaseModel):
@@ -43,7 +15,7 @@ class BaseAPIRequest(BaseModel):
     uid: int
 
     def get_client(self) -> genshin.Client:
-        return GenshinClient(
+        return genshin.Client(
             self.cookies,
             lang=self.lang,
             region=genshin.Region(self.region),
